3271035ff859c7b13ee01133606d57c0328c078d
Stephanie <stephaniereyes@pursuit.org>
2024-05-19 14:28:16 -0400

    fixed css so there can be 4 items in a row

--- pages/ProductList.jsx
+++ pages/ProductList.jsx
@@ -22,14 +22,16 @@ const ProductList = () => {
 
   return (
     <div className="page-container" style={{ textAlign: 'center', padding: '20px', backgroundColor: '#edf7f6', color: '#56351e' }}>
-      <HeaderTop />
+     <HeaderTop />
+      
       <div className="productListPage">
       
     </div>
-      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '20px' }}>
+      <div className="products-grid">
         {items.map(item => (
           <ProductCard key={item.id} item={item} />
         ))}
+
       </div>
       <footer className="footer">
        
